ea8eaec896ac163d6917f971feb243ef3a0a9f25
Vladimir Moreira <vladimir.giomoreira@gmail.com>
2026-04-21 21:47:40 -0300

    Actualizar HTTPS

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+}); 
 
 // servidor HTTPS
 https.createServer(sslOptions, app).listen(PORT, () => {
